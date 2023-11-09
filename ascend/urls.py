c087335976592ba3f0aedb66244eaffa93f351a4
Treasure Mayowa <treasuremayowa@outlook.com>
2023-11-09 18:33:57 +0100

    Add authentication views, urls

--- ascend/urls.py
+++ ascend/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home")
+    path('', views.index, name="home"),
+    path('sign-up', views.register, name="register"),
+    path('login', views.login_view, name="login"),
+    path('logout', views.logout_view, name="logout")
 ]
--- ascend/views.py
+++ ascend/views.py
@@ -1,5 +1,69 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .models import *
+
 
 # Create your views here.
 def index(request):
     return render(request, 'ascend/index.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, "ascend/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "ascend/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("home"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        if request.POST["school"]:
+            school = request.POST["school"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+
+        if password != confirmation:
+            return render(request, "ascend/register.html", {
+                "message": "Passwords must match."
+            })
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            if school:
+                user.school = school
+            user.save()
+        except IntegrityError:
+            return render(request, "ascend/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("home"))
+    else:
+        return render(request, "ascend/register.html")
