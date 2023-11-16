31d85f31569db130d0a03b3dbafcdc6a846df5a3
Treasure Mayowa <treasuremayowa@outlook.com>
2023-11-16 01:37:37 +0100

    Register new model

--- ascend/models.py
+++ ascend/models.py
@@ -4,3 +4,20 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     school = models.TextField(blank=True)
+
+class YoutubeContent(models.Model):
+    CATEGORY_CHOICES = [
+        ("Classroom Management", "Classroom Management"),
+        ("Technology Integration", "Technology Integration"),
+        ("Assessment and Feedback", "Assessment and Feedback"),
+        ("Parents Engagement", "Parents Engagement"),
+        ("Students Engagement", "Students Engagement"),
+        ("Inclusion", "Inclusion"),
+        ("Real-world skills", "Real-world skills"),
+        ("General Tips", "General Tips")
+    ]
+
+    title = models.TextField()  
+    embed_code = models.TextField()
+    source = models.TextField()
+    category = models.TextField(choices=CATEGORY_CHOICES, null=True)
